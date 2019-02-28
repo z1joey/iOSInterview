42fdebcaadce5cb706486fe9403434a54af81719
Yi Zhang <yizhang@macdemac-mini.local>
2019-02-28 22:07:49 +0800

    Initial Commit

---
+++ iOSInterview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSInterview
+//
+//  Created by Yi Zhang on 2019/2/28.
+//  Copyright © 2019 Yi Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSInterview/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  iOSInterview
+//
+//  Created by Yi Zhang on 2019/2/28.
+//  Copyright © 2019 Yi Zhang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
